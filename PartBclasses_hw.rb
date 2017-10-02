f334fbb075c049f06773c07aadb4a251469767a5
Miguel <miguelmoutela@hotmail.com>
2017-10-02 18:13:02 +0100

    refactored code

--- PartBclasses_hw.rb
+++ PartBclasses_hw.rb
@@ -1,5 +1,9 @@
 class Team
 
+  attr_reader(:name :players :coach)
+  #players and coaches come and go; team names do not change
+  attr_accessor(:players :coach)
+
   def initialize(input_name, input_players, input_coach)
     @name = input_name
     @players = input_players
@@ -7,33 +11,33 @@ class Team
   end
 
 
- # GETTER METHODS
-
-  def name
-
-    return @name
-
-  end
-
-  def players
-
-    return @players
-
-  end
-
-  def coach
-
-    return @coach
-
-  end
-
-# GETTER METHODS
-
-  def set_coach(coach)
-
-    @coach = coach
-
-  end
+#  # GETTER METHODS
+#
+#   def name
+#
+#     return @name
+#
+#   end
+#
+#   def players
+#
+#     return @players
+#
+#   end
+#
+#   def coach
+#
+#     return @coach
+#
+#   end
+#
+# # GETTER METHODS
+#
+#   def set_coach(coach)
+#
+#     @coach = coach
+#
+#   end
 
 
 end
